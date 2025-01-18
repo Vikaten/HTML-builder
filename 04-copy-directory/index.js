f172571e3272ab4cfef7b96f8f2d714b83815b9f
Temnova Vika <vikaten06@gmail.com>
2025-01-18 13:54:06 +0300

    feat: solve the 4 task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+async function copyFiles() {
+  await fs.mkdir('04-copy-directory/files-copy', { recursive: true });
+  const files = await fs.readdir('04-copy-directory/files');
+  for (let file of files) {
+    const copyFile = '04-copy-directory/files/' + file;
+    const pasteFile = '04-copy-directory/files-copy/' + file;
+    await fs.copyFile(copyFile, pasteFile);
+  }
+  console.log('Данные успешно скопировались!');
+}
+
+copyFiles();

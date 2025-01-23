47217167bbd3acfaaa07b976f31135bbd57e82c8
Temnova Vika <vikaten06@gmail.com>
2025-01-23 10:07:09 +0300

    fix: delete deleted files from the source folder when copying from the target folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,8 +2,17 @@ const fs = require('fs').promises;
 
 async function copyFiles() {
   await fs.mkdir('04-copy-directory/files-copy', { recursive: true });
-  const files = await fs.readdir('04-copy-directory/files');
-  for (let file of files) {
+  const filesSource = await fs.readdir('04-copy-directory/files');
+  const filesTarget = await fs.readdir('04-copy-directory/files-copy');
+  for (let file of filesTarget) {
+    await fs.unlink('04-copy-directory/files-copy/' + file, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Файл успешно удалён');
+    });
+  }
+  for (let file of filesSource) {
     const copyFile = '04-copy-directory/files/' + file;
     const pasteFile = '04-copy-directory/files-copy/' + file;
     await fs.copyFile(copyFile, pasteFile);
